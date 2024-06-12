e139dc5090059e6a5e9fe538995bba1ae2dcd47d
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-06-12 18:46:10 +0500

    creating dashboard page last commit

---
+++ src/Components/IssuesTable.jsx
@@ -0,0 +1,28 @@
+
+
+
+function IssuesTable(props) {
+
+  
+
+    return (
+      <>
+            <div className='flex items-center py-[13px] px-[22px] border-b border-[#E5E6EE] '>
+                <div className='flex-[3] text-[#000000] text-[13px] font-[300] flex items-center gap-[10px] '>
+                    <p>{props.typeOfIssue}</p>
+                    <span className={` ${props.orange ? 'bg-[#FFF2E5] text-[#F27A23]' : 'bg-[#FFEEEE] text-[#FF4954]'} font-[600] text-[12px] px-[14px] py-[4px] rounded-[30px] `}>
+                        errors
+                    </span>
+                </div>
+                <div className='flex-1 text-[#000000] text-[13px] font-[300] '>
+                    {props.numberOfIssue}
+                </div>
+                <div className='flex-1 text-[#0D99FF] text-[13px] font-[300] '>
+                    {props.aboutTheIssue}
+                </div>
+            </div>
+      </>
+    )
+  }
+  
+  export default IssuesTable
