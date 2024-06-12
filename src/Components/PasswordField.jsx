da0bbe8dcef207dc2be14642c17f6717b31ffc9a
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-06-12 12:38:58 +0500

    password hide show in state

--- src/Components/PasswordField.jsx
+++ src/Components/PasswordField.jsx
@@ -7,18 +7,16 @@ import React , {useState} from 'react'
 function PasswordField() {
 
     const [type, setType] = useState('password');
-    let eyeIcon = document.querySelector('.eye_icon');
+    const [hide, setShow] = useState('fa-regular fa-eye-slash');
     // setType('text');
 
     const changeType = () => {
         if (type === 'password') {
             setType('text');
-            eyeIcon.classList.remove('fa-eye-slash');
-            eyeIcon.classList.add('fa-eye');
+            setShow('fa-regular fa-eye');
         }else{
             setType('password');
-            eyeIcon.classList.remove('fa-eye');
-            eyeIcon.classList.add('fa-eye-slash');
+            setShow('fa-regular fa-eye-slash');
         }
     }
 
@@ -30,7 +28,7 @@ function PasswordField() {
         <div className="flex-1 w-full relative " >
             <input placeholder='Enter your company password' className='flex-1 w-full outline-none border rounded-[6px] text-[14px] text-[#000000] border-[#E3E2E9] px-[13px] py-[11px]  mt-[4px] ' type={type} required name="" id='password' />
                 <span className="flex items-center justify-center px-[14px] h-full absolute cursor-pointer right-0 top-[3px] " onClick={changeType} >
-                    <i class="fa-regular fa-eye-slash text-[#807D8D] eye_icon "></i>
+                    <i class={`${hide} text-[#807D8D] eye_icon `} ></i>
                 </span>
         </div>
       </>
