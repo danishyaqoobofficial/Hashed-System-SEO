294675b442442d90a83a50d1e5bddada339a326a
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-06-20 16:52:15 +0500

    all buges fixes in dashboard page

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -22,6 +22,7 @@ function Dashboard() {
     const [messageCounter, setMessageCounter] = useState('block');
     const [profilePopup, setProfilePopup] = useState('hidden');
     const [rotate, setRotate] = useState('rotate-180');
+    const [border, setBorder] = useState('border-transparent');
 
     const notificationPopup = ()=>  {
         if (popup === 'hidden') {
@@ -36,10 +37,21 @@ function Dashboard() {
         if (profilePopup === 'hidden') {
             setProfilePopup('block');
             setRotate('rotate-270');
+            setBorder('border-[#b5bfd9]');
         }else{
             setProfilePopup('hidden');
             setRotate('rotate-180');
         }
+    }
+
+    const CloseProfilePopup = () =>{
+        setProfilePopup('hidden');
+        setRotate('rotate-180');
+        setBorder('border-transparent');
+    }
+
+    const CloseNotificationPopup = () =>{
+        setPopup('hidden');
     }
 
   
@@ -60,7 +72,7 @@ function Dashboard() {
                 </span>
             </aside>
             <main className='flex-1 ' >
-                <nav className='flex items-center justify-between  bg-[#fff] sticky top-0 px-[22px] py-[11px] border-b border-[#E5E6EE] w-full ' >
+                <nav className='flex items-center justify-between  bg-[#fff] sticky top-0 px-[16px] sm:px-[22px] py-[11px] border-b border-[#E5E6EE] w-full ' >
                     <span className='flex items-center justify-center cursor-pointer '>
                         <svg width="35" height="34" viewBox="0 0 35 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect width="35" height="34" fill="white"/>
@@ -69,9 +81,9 @@ function Dashboard() {
                             <line x1="10" y1="23.5" x2="25" y2="23.5" stroke="#2B3674"/>
                         </svg>
                     </span>
-                    <div className='flex items-center gap-[22px] '>
+                    <div className='flex items-center gap-[16px] sm:gap-[22px] '>
                         <div className=' relative '>
-                            <div className=' relative cursor-pointer ' onClick={notificationPopup} >
+                            <div className=' relative cursor-default ' onClick={notificationPopup} >
                                 <svg width="26" height="26" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <g clip-path="url(#clip0_47_8013)">
                                     <path d="M25.7193 23.5533L23.9993 21.8333V15.1667C23.9993 11.0733 21.8127 7.64666 17.9993 6.73999V5.83333C17.9993 4.72666 17.106 3.83333 15.9993 3.83333C14.8927 3.83333 13.9993 4.72666 13.9993 5.83333V6.73999C10.1727 7.64666 7.99934 11.06 7.99934 15.1667V21.8333L6.27934 23.5533C5.43934 24.3933 6.026 25.8333 7.21267 25.8333H24.7727C25.9727 25.8333 26.5593 24.3933 25.7193 23.5533ZM21.3327 23.1667H10.666V15.1667C10.666 11.86 12.6793 9.16666 15.9993 9.16666C19.3193 9.16666 21.3327 11.86 21.3327 15.1667V23.1667ZM15.9993 29.8333C17.466 29.8333 18.666 28.6333 18.666 27.1667H13.3327C13.3327 28.6333 14.5193 29.8333 15.9993 29.8333Z" fill="#A3AED0"/>
@@ -88,23 +100,9 @@ function Dashboard() {
                                     </span>
                                 </span>
                             </div>
-                            <div className={` absolute w-[300px] max-h-[320px] overflow-auto bg-[#fff] rounded-[11px] top-[28px] right-[10px] Shadow ${popup} `}>
-                                <div className='border-b border-[#E4E4E4] px-[22px] py-[11px] flex items-start gap-[9px] '>
-                                    <i class="fa-regular fa-message text-[18px] text-[#0000006c] pt-[1px] "></i>
-                                    <p className='text-[13px] text-[#0000006c] font-[500]'>You a new message notification. Open it and start your working.</p>
-                                </div>
-                                <div className='border-b border-[#E4E4E4] px-[22px] py-[11px] flex items-start gap-[9px] '>
-                                    <i class="fa-regular fa-message text-[18px] text-[#0000006c] pt-[1px] "></i>
-                                    <p className='text-[13px] text-[#0000006c] font-[500]'>You a new message notification. Open it and start your working.</p>
-                                </div>
-                                <div className='border-b border-[#E4E4E4] px-[22px] py-[11px] flex items-start gap-[9px] '>
-                                    <i class="fa-regular fa-message text-[18px] text-[#0000006c] pt-[1px] "></i>
-                                    <p className='text-[13px] text-[#0000006c] font-[500]'>You a new message notification. Open it and start your working.</p>
-                                </div>
-                            </div>
                         </div>
                         <div className=' relative '>
-                            <div className='flex items-center gap-[10px] cursor-default border border-transparent hover:border-[#b5bfd9] transition-all rounded-[30px] p-[5px] ' onClick={ProfilePopup} >
+                            <div className={`flex items-center gap-[10px] cursor-default border border-transparent hover:border-[#b5bfd9] transition-all rounded-[30px] p-[5px] ${border} `} onClick={ProfilePopup} >
                                 <div className='flex items-center gap-[7px] '>
                                     <div className=' bg-cover bg-no-repeat bg-center h-[34px] w-[34px] rounded-[100px] ' style={{backgroundImage : `url(${DashboardImage})` }} ></div>
                                     <p className='text-[#1E2549] text-[14px] font-[500] select-none '>John Smith</p>
@@ -112,12 +110,6 @@ function Dashboard() {
                                 <span>
                                     <i class={`fa-solid fa-angle-up text-[14px] text-[#9e9ca8] transition-all ${rotate} `}></i>
                                 </span>
-                            </div>
-                            <div className={` absolute max-w-[200px] max-h-[220px] overflow-auto bg-[#fff] rounded-[11px] top-[52px] right-0 Shadow p-[6px] transition-all ${profilePopup} `}>
-                                <div className='flex items-center justify-center gap-[13px] rounded-[9px] bg-transparent text-[#F10A0A] hover:bg-red-100 cursor-pointer px-[14px] py-[12px]  '>
-                                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 17.3555L14 17.3555C14.7956 17.3555 15.5587 17.0394 16.1213 16.4768C16.6839 15.9142 17 15.1511 17 14.3555L17 4.35547C17 3.55982 16.6839 2.79676 16.1213 2.23415C15.5587 1.67154 14.7956 1.35547 14 1.35547L13 1.35547M5 5.35547L1 9.35547M1 9.35547L5 13.3555M1 9.35547L13 9.35547" stroke="#F10A0A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-                                    Log Out
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -287,7 +279,7 @@ function Dashboard() {
                             <h2 className='text-[22px] text-[#000000] font-[500] mt-[16px] '>Top Issues</h2>
 
                             {/*  */}
-                            <div className='mt-[16px] flex-1 w-full rounded-[9px] overflow-auto border border-[#E5E6EE] pb-[20px] bg-white '>
+                            {/* <div className='mt-[16px] flex-1 w-full rounded-[9px] overflow-auto border border-[#E5E6EE] pb-[20px] bg-white '>
                                 <div className='flex items-center py-[9px] px-[22px] bg-[#E5E6EE] border-b border-[#E5E6EE] gap-[8px] '>
                                     <div className='flex-[3] text-[#000000] text-[13px] font-[300] '>
                                         Type of issues
@@ -308,10 +300,10 @@ function Dashboard() {
                                 <IssuesTable typeOfIssue = 'Incorrect pages found in sitemap' numberOfIssue = '91' aboutTheIssue = 'Why and how to fix it' />
                                 <IssuesTable typeOfIssue = '4xx errors' numberOfIssue = '91' aboutTheIssue = 'Why and how to fix it' />
                                 <IssuesTable typeOfIssue = '4xx errors' numberOfIssue = '91' aboutTheIssue = 'Why and how to fix it' />
-                            </div>
+                            </div> */}
                         </div>
                         <div className=' '>
-                            <div className='w-[270px] border border-[#E5E6EE] rounded-[6px] bg-white p-[18px] '>
+                            <div className='w-[226px] sm:[270px] border border-[#E5E6EE] rounded-[6px] bg-white p-[18px] '>
                                 <div className='flex items-center gap-[6px] '>
                                     <h2 className='text-[16px] text-[#000000] font-[500]'>Site Health</h2>
                                     <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -352,7 +344,7 @@ function Dashboard() {
                                     </div>
                                 </div>
                             </div>
-                            <div className='w-[270px] border border-[#E5E6EE] rounded-[6px] bg-white p-[18px] mt-[22px] '>
+                            <div className='w-[226px] sm:w-[270px] border border-[#E5E6EE] rounded-[6px] bg-white p-[18px] mt-[22px] '>
                                 <h2 className='text-[16px] text-[#000000] font-[500]'>Crawled Pages</h2>
                                 <div className='h-[36px] flex items-center mt-[12px] '>
                                     <span className='h-full w-[5%] bg-[#00C58F] '></span>
@@ -396,7 +388,7 @@ function Dashboard() {
                                     <p className='text-[13px] text-[#000000] font-[300] '>0</p>
                                 </div>
                             </div>
-                            <div className='w-[270px] border border-[#E5E6EE] rounded-[6px] bg-white p-[18px] mt-[22px] '>
+                            <div className='w-[226px] sm:w-[270px] border border-[#E5E6EE] rounded-[6px] bg-white p-[18px] mt-[22px] '>
                                 <div className='border-b border-[#E4E4E4] pb-[14px] '>
                                     <div className='flex items-center gap-[6px] '>
                                         <h2 className='text-[16px] text-[#000000] font-[500]'>Robots.txt Updates</h2>
@@ -424,6 +416,32 @@ function Dashboard() {
                 </section>
             </main>
         </main>
+        <div className={` fixed bg-transparent !h-[100vh] inset-0 flex justify-end items-start pt-[60px] px-[22px] ${profilePopup} `} onClick={CloseProfilePopup} >
+            <Link to={'/'} className={` max-w-[140px] max-h-[220px] overflow-auto bg-[#fff] rounded-[11px] Shadow p-[5px] transition-all  `}>
+                <div className='flex items-center justify-center gap-[13px] rounded-[9px] bg-transparent text-[#F10A0A] hover:bg-red-100 cursor-pointer px-[14px] py-[12px]  '>
+                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 17.3555L14 17.3555C14.7956 17.3555 15.5587 17.0394 16.1213 16.4768C16.6839 15.9142 17 15.1511 17 14.3555L17 4.35547C17 3.55982 16.6839 2.79676 16.1213 2.23415C15.5587 1.67154 14.7956 1.35547 14 1.35547L13 1.35547M5 5.35547L1 9.35547M1 9.35547L5 13.3555M1 9.35547L13 9.35547" stroke="#F10A0A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    Log Out
+                </div>
+            </Link>
+        </div>
+
+        {/*  */}
+        <div className={` fixed bg-transparent !h-[100vh] inset-0 flex justify-end items-start pt-[52px] pr-[12px] sm:pr-[210px] ${popup} `} onClick={CloseNotificationPopup} >
+            <div className={` w-[300px] max-h-[320px] overflow-auto bg-[#fff] rounded-[11px] top-[28px] right-[10px] Shadow  `}>
+                <div className='border-b border-[#E4E4E4] px-[22px] py-[11px] flex items-start gap-[9px] '>
+                    <i class="fa-regular fa-message text-[18px] text-[#0000006c] pt-[1px] "></i>
+                    <p className='text-[13px] text-[#0000006c] font-[500]'>You a new message notification. Open it and start your working.</p>
+                </div>
+                <div className='border-b border-[#E4E4E4] px-[22px] py-[11px] flex items-start gap-[9px] '>
+                    <i class="fa-regular fa-message text-[18px] text-[#0000006c] pt-[1px] "></i>
+                    <p className='text-[13px] text-[#0000006c] font-[500]'>You a new message notification. Open it and start your working.</p>
+                </div>
+                <div className='border-b border-[#E4E4E4] px-[22px] py-[11px] flex items-start gap-[9px] '>
+                    <i class="fa-regular fa-message text-[18px] text-[#0000006c] pt-[1px] "></i>
+                    <p className='text-[13px] text-[#0000006c] font-[500]'>You a new message notification. Open it and start your working.</p>
+                </div>
+            </div>
+        </div>
       </>
     )
   }
